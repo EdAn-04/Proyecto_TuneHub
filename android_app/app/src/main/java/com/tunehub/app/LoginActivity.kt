02c5db4c8c78326d3ea9d7f058981ddc19075d44
EdAn-04 <andres.culajay@galileo.edu>
2026-04-06 13:17:37 -0600

    Navegación Login a Dashboard implementada

--- android_app/app/src/main/java/com/tunehub/app/LoginActivity.kt
+++ android_app/app/src/main/java/com/tunehub/app/LoginActivity.kt
@@ -5,9 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
